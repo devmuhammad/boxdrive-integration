86b218185933b24eb805637bd1229eb6523abf9d
Muhammad <muhammad.s@comsoftltd.com>
2018-04-27 15:10:01 +0100

    folders

--- CONFIG/index.js
+++ CONFIG/index.js
@@ -1,5 +1,10 @@
 module.exports = {
   app: {
     port: process.env.PORT || 6000 
+  },
+  dropbox: {
+    appkey: 'aot6dtkqo5lwpqf',
+    appsecret: 'whhgt9elc95qa12',
+    accesstoken: 'Kg5cBu5JYKAAAAAAAAAASO2epXMNnV2RiZCmYS9rUZyGXbl5OLDN-wBOai3DrwXZ'
   }
 }
