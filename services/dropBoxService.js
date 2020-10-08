6f60cc3c1f456d5eadc60f0fe3073f35b84758ee
Muhammad Olacoded <38692292+devmuhammad@users.noreply.github.com>
2020-10-08 15:32:42 +0100

    Update dropBoxService.js

--- services/dropBoxService.js
+++ services/dropBoxService.js
@@ -20,3 +20,15 @@ const config = require('../config')
         SESSION_ID_SECRET:'com12-acuity-drive',
      }
 
+
+
+
+
+
+
+dropbox: {
+    appkey: 'aot6dtkqo5lwpqf',
+    appsecret: 'whhgt9elc95qa12',
+    accesstoken: 'Kg5cBu5JYKAAAAAAAAAASO2epXMNnV2RiZCmYS9rUZyGXbl5OLDN-wBOai3DrwXZ',
+    SESSION_ID_SECRET:'com12-acuity-drive',
+  }
