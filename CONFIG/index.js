7b0bc11e990e021bf9fa6a8256006b958e9ac9c4
Muhammad Olacoded <38692292+devmuhammad@users.noreply.github.com>
2020-10-08 15:31:30 +0100

    Update index.js

--- CONFIG/index.js
+++ CONFIG/index.js
@@ -2,10 +2,5 @@ module.exports = {
   app: {
     port: process.env.PORT || 8090 
   },
-  dropbox: {
-    appkey: 'aot6dtkqo5lwpqf',
-    appsecret: 'whhgt9elc95qa12',
-    accesstoken: 'Kg5cBu5JYKAAAAAAAAAASO2epXMNnV2RiZCmYS9rUZyGXbl5OLDN-wBOai3DrwXZ',
-    SESSION_ID_SECRET:'com12-acuity-drive',
-  }
+  
 }
